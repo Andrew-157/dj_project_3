df6825ce698ea34261317fc6089b9d4e6eea8145
Andrew Subotin <subotinandrey5@gmail.com>
2023-06-13 18:02:41 +0300

    Movies by genre view

--- movies/urls.py
+++ movies/urls.py
@@ -4,5 +4,7 @@ from movies import views
 
 app_name = 'movies'
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index')
+    path('', views.IndexView.as_view(), name='index'),
+    path('movies/<str:genre_slug>/',
+         views.MoviesByGenreListView.as_view(), name='genre-movies')
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,9 +1,31 @@
+from typing import Any, Dict
+from django.db.models.query import QuerySet
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 from django.views.generic import ListView
 from taggit.models import Tag
+from movies.models import Movie, Director
 
 
 class IndexView(ListView):
     template_name = 'movies/index.html'
     context_object_name = 'genres'
     queryset = Tag.objects.order_by('name').all()
+
+
+class MoviesByGenreListView(ListView):
+    template_name = 'movies/movies_by_genre.html'
+    context_object_name = 'movies'
+
+    def get_queryset(self) -> QuerySet[Any]:
+        genre_slug = self.kwargs['genre_slug']
+        genre = Tag.objects.filter(slug=genre_slug).first()
+        movies = Movie.objects.filter(genres=genre).\
+            prefetch_related('genres').\
+            select_related('director').all()
+        return movies
+
+    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context['genre'] = self.kwargs['genre_slug']
+        return context
