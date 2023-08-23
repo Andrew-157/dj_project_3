7bc5ce2759ff0bdea5ae245c58c4ffcab5f4bf9d
Andrew Subotin <subotinandrey5@gmail.com>
2023-08-23 15:58:44 +0200

    Updated tests for 'users' app

--- users/tests/test_views.py
+++ users/tests/test_views.py
@@ -1,8 +1,10 @@
+from django.contrib import auth
 from django.contrib.messages import get_messages
 from django.test import TestCase
 from django.urls import reverse
 
 from users.models import CustomUser
+from users.forms import UserCreationForm, EmailLoginForm, UserChangeForm
 
 
 class RegisterUserViewTest(TestCase):
@@ -16,6 +18,7 @@ class RegisterUserViewTest(TestCase):
         response = self.client.get(reverse('users:register'))
         self.assertEqual(response.status_code, 200)
         self.assertTrue('form' in response.context)
+        self.assertTrue(isinstance(response.context['form'], UserCreationForm))
 
     def test_correct_response_if_invalid_data_posted(self):
         response = self.client.post(reverse('users:register'),
@@ -25,18 +28,26 @@ class RegisterUserViewTest(TestCase):
                                           'password2': '34password34'})
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'users/register.html')
+        error_message = b'Username cannot be shorter than 6 characters.'
+        self.assertTrue(error_message in response.content)
 
     def test_correct_response_if_empty_data_posted(self):
         response = self.client.post(reverse('users:register'),
                                     data={})
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'users/register.html')
+        error_message = b'This field is required.'
+        self.assertTrue(error_message in response.content)
+        self.assertEqual(response.content.count(error_message), 4)
 
     def test_correct_response_if_no_data_posted(self):
         response = self.client.post(reverse('users:register'),
                                     data=None)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'users/register.html')
+        error_message = b'This field is required.'
+        self.assertTrue(error_message in response.content)
+        self.assertEqual(response.content.count(error_message), 4)
 
     def test_correct_response_if_successful_registration(self):
         response = self.client.post(reverse('users:register'),
@@ -50,6 +61,8 @@ class RegisterUserViewTest(TestCase):
         self.assertEqual(str(messages[0]), 'You were successfully registered.')
         new_user = CustomUser.objects.filter(username='user12').first()
         self.assertTrue(new_user is not None)
+        user = auth.get_user(self.client)
+        self.assertTrue(user.is_authenticated)
 
 
 class LoginUserViewTest(TestCase):
@@ -68,6 +81,7 @@ class LoginUserViewTest(TestCase):
         response = self.client.get(reverse('users:login'))
         self.assertEqual(response.status_code, 200)
         self.assertTrue('form' in response.context)
+        self.assertTrue(isinstance(response.context['form'], EmailLoginForm))
 
     def test_correct_response_if_invalid_data_posted(self):
         response = self.client.post(reverse('users:login'),
@@ -89,6 +103,34 @@ class LoginUserViewTest(TestCase):
         self.assertTrue(response.wsgi_request.user.is_authenticated)
 
 
+class LogoutViewTest(TestCase):
+
+    def test_correct_response_to_not_logged_user(self):
+        response = self.client.get(reverse('users:logout'))
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse('movies:index'))
+        messages = list(get_messages(response.wsgi_request))
+        self.assertEqual(str(messages[0]), 'You have successfully logged out')
+
+    def test_correct_response_to_logged_user(self):
+        username = 'new_user'
+        password = '34somepassword34'
+        CustomUser.objects.create_user(
+            username=username,
+            email='new_user@gmail.com',
+            password=password
+        )
+        login = self.client.login(username=username,
+                                  password=password)
+        response = self.client.get(reverse('users:logout'))
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse('movies:index'))
+        messages = list(get_messages(response.wsgi_request))
+        self.assertEqual(str(messages[0]), 'You have successfully logged out')
+        user = auth.get_user(self.client)
+        self.assertFalse(user.is_authenticated)
+
+
 class ChangeUserViewTest(TestCase):
     @classmethod
     def setUpTestData(cls) -> None:
@@ -98,6 +140,9 @@ class ChangeUserViewTest(TestCase):
 
     def test_correct_redirect_for_not_logged_user(self):
         response = self.client.get(reverse('users:change-user'))
+        self.assertEqual(response.status_code, 302)
+        self.assertTrue(response.url.startswith('/become_user/'))
+        response = self.client.post(reverse('users:change-user'))
         self.assertEqual(response.status_code, 302)
         self.assertTrue(response.url.startswith('/become_user/'))
 
@@ -114,6 +159,7 @@ class ChangeUserViewTest(TestCase):
         response = self.client.get(reverse('users:change-user'))
         self.assertEqual(response.status_code, 200)
         self.assertTrue('form' in response.context)
+        self.assertTrue(isinstance(response.context['form'], UserChangeForm))
 
     def test_correct_response_if_invalid_data_posted(self):
         login = self.client.login(username='some_user',
@@ -123,6 +169,8 @@ class ChangeUserViewTest(TestCase):
                                           'email': 'invalid'})
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'users/change_user.html')
+        error_message = b'Username cannot be shorter than 6 characters.'
+        self.assertTrue(error_message in response.content)
 
     def test_correct_response_if_empty_data_posted(self):
         login = self.client.login(username='some_user',
@@ -131,6 +179,9 @@ class ChangeUserViewTest(TestCase):
                                     data={})
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'users/change_user.html')
+        error_message = b'This field is required.'
+        self.assertTrue(error_message in response.content)
+        self.assertEqual(response.content.count(error_message), 2)
 
     def test_correct_response_if_no_data_posted(self):
         login = self.client.login(username='some_user',
@@ -139,6 +190,9 @@ class ChangeUserViewTest(TestCase):
                                     data=None)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'users/change_user.html')
+        error_message = b'This field is required.'
+        self.assertTrue(error_message in response.content)
+        self.assertEqual(response.content.count(error_message), 2)
 
     def test_redirect_if_valid_data_posted(self):
         login = self.client.login(username='some_user',
